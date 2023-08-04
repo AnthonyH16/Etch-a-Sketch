2043e10295176ef3fae7a690dfd23e657bf19697
AnthonyH16 <132026902+AnthonyH16@users.noreply.github.com>
2023-08-04 11:44:59 -0400

    create nested loop for rows and columns create function to remove current grid to then populate new grid hover effect

--- etch.js
+++ etch.js
@@ -1,6 +1,7 @@
 let userInput;
 const button = document.querySelector('#start');
 button.addEventListener('click', () => {
+    byeBye();
     userInput = prompt("How many?");
     makeDivs(userInput);
 });
@@ -14,11 +15,28 @@ function makeDivs(){
     }
     else if (userInput <= 100) {
         for (i = 0 ; i < userInput ; i++){
-          const grid = document.createElement('div');
-            
-            container.appendChild(grid);
+          const column = document.createElement('div');
+          column.classList.add('column');
+          for (j = 0 ; j < userInput ; j++){
+            const row = document.createElement('div');
+              row.classList.add('row');
+              column.appendChild(row);
+              row.addEventListener('mouseover', (event) => {
+                event.target.style.backgroundColor ="orange";
+            });
+          }
+            container.appendChild(column);
             
         }
+        
     }
 
 }
+
+function byeBye(){
+    const parent = document.querySelector('#container');
+    while (parent.firstChild){
+        parent.firstChild.remove();
+    }
+}
+
